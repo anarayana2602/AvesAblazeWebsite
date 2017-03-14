3c72c6dbd099fc2cb53b7edf6839e1b93fce6fa6
THE-COB <jmlife6@gmail.com>
2017-03-14 15:06:30 -0400

    Delete homeScript.js

--- homeScript.js
+++
@@ -1,9 +0,0 @@
-var fade = function(){
-	var images = ["teamPhoto.jpg"];
-	for(count = 0; count < images.length; count++){
-		document.getElementById("homePhoto").src = images[count];
-		for(opacity = 0; opacity <= 1; opacity += 0.001){
-			document.getElementById("homePhoto").opacity = opacity;
-		}
-	}
-}
